dae7073147d80a4db45da9a9255c104633282bd7
Mohd Azfar <62957738+mo-azfar@users.noreply.github.com>
2023-10-28 16:05:00 +0800

    new

---
+++ Custom/Kernel/Output/HTML/FilterElementPost/ShowAgentLoginBG.pm
@@ -0,0 +1,92 @@
+# --
+# Copyright (C) 2023 mo-azfar,https://github.com/mo-azfar
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::Output::HTML::FilterElementPost::ShowAgentLoginBG;
+
+use strict;
+use warnings;
+
+our @ObjectDependencies = (
+    'Kernel::Config',
+);
+
+use Kernel::System::VariableCheck qw(:all);
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    my $Self = {};
+    bless( $Self, $Type );
+
+    return $Self;
+}
+
+sub Run {
+    my ( $Self, %Param ) = @_;
+
+    my $ConfigObject                    = $Kernel::OM->Get('Kernel::Config');
+    #my $ParamObject                     = $Kernel::OM->Get('Kernel::System::Web::Request');
+	
+	# --
+	# Agent Login Background
+	# --
+	if ( $Param{TemplateFile} eq 'Login')
+    {
+        if ( defined $ConfigObject->Get('AgentLoginBackground') ) 
+        {
+            my %AgentLoginBackground = %{ $ConfigObject->Get('AgentLoginBackground') };
+            my %Data;
+
+            for my $CSSStatement ( sort keys %AgentLoginBackground ) {
+                if ( $CSSStatement eq 'BackgroundURL' ) {
+                    my $WebPath = '';
+                    if ( $AgentLoginBackground{$CSSStatement} !~ /(http|ftp|https):\//i ) {
+                        $WebPath = $ConfigObject->Get('Frontend::WebPath');
+                    }
+                    $Data{'BackgroundURL'} = 'url(' . $WebPath . $AgentLoginBackground{$CSSStatement} . ')';
+                }
+                else {
+                    $Data{$CSSStatement} = $AgentLoginBackground{$CSSStatement};
+                }
+            }
+
+            my $CSS = qq~<style type="text/css">
+            .LoginBG {
+                width: $Data{'BackgroundWidth'};
+                height: $Data{'BackgroundHeight'};
+                background: $Data{'BackgroundURL'};
+                background-position: $Data{'BackgroundPosition'};
+                background-repeat: $Data{'BackgroundRepeat'};
+                background-attachment: $Data{'BackgroundAttachment'};
+                background-size: $Data{'BackgroundSize'};
+            }
+            </style>
+            ~;
+
+             #set background class on app wrapper
+            my $SearchField1 = quotemeta "<div id=\"AppWrapper\">";
+            my $ReturnField1 = qq~$CSS <div id="AppWrapper" class="LoginBG">
+            ~;
+
+            #set mainbox to have transparent background
+            my $SearchField2 = quotemeta " <div class=\"MainBox ARIARoleMain\">";
+            my $ReturnField2 = qq~ <div class="MainBox ARIARoleMain" style="background:transparent;">
+            ~;
+        
+            #search and replace	 
+            ${ $Param{Data} } =~ s{$SearchField1}{$ReturnField1};
+            ${ $Param{Data} } =~ s{$SearchField2}{$ReturnField2};
+        }        
+		
+    }  
+	# --
+	
+    return 1;
+}
+
+1;
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Znuny-Agent-Login-Background
- Show agent login backround (image)
+- Show agent login background (image)
+- Based on Znuny 7.0.x
+ 
+1. Upload your image at /opt/znuny/var/httpd/htdocs/skins/Agent/default/img/
+2. Update Admin > System Configuration > AgentLoginBackground
+
+![agent-login-bg](agent-login-bg.png)
